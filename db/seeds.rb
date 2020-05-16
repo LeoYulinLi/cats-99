03435d7962bf9ba3f79b2ca1539d5dee8c07dd80
LeoYulinLi <LeoYulinLi@gmail.com>
2020-05-15 17:02:03 -0700

    Checkpoint for day 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cat.create(name: 'Spike', birth_date: '2015/01/20', color: 'brown', sex: 'M', description: "It's a cat")
+
+Cat.create(name: 'Sakana', birth_date: '2017/01/21', color: 'black', sex: 'M', description: "It's a fish")
+
+Cat.create(name: 'Rocky', birth_date: '2011/03/10', color: 'orange', sex: 'F', description: "It's another cat")
